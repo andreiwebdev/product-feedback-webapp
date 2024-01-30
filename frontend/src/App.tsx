befe6e6698760ea612bae2ba86e7959ea11b48cf
Andrei <dandrei.web@gmail.com>
2024-01-30 20:06:22 +0100

    Update: App Structure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,9 @@
-import { RoadmapScreen } from "./components/RoadmapScreen";
+import { SuggestionsScreen } from "./components/SuggestionsScreen";
 
 function App() {
     return (
         <main className="bg-lightGrey w-full h-full min-h-screen md:pt-[56px] pb-[88px]">
-            <RoadmapScreen />
+            <SuggestionsScreen />
         </main>
     );
 }
