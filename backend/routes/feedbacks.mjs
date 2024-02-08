c12098d8307ab5689edc7cc99e543460c508b7dd
Andrei <dandrei.web@gmail.com>
2024-02-08 20:46:00 +0100

    Update: backend feedbacks votes

--- backend/routes/feedbacks.mjs
+++ backend/routes/feedbacks.mjs
@@ -1,5 +1,6 @@
 import express from "express";
 import db from "../db/conn.mjs";
+import mongodb from "mongodb";
 
 const router = express.Router();
 
@@ -21,6 +22,30 @@ router.post("/", async (req, res) => {
     newDocument.date = new Date();
     let result = await collection.insertOne(newDocument);
     res.send(result).status(204);
+});
+
+router.patch("/:id/vote", async (req, res) => {
+    const { id } = req.params;
+    const { vote } = req.body;
+
+    try {
+        const feedback = await db
+            .collection("feedbacks")
+            .findOneAndUpdate(
+                { _id: new mongodb.ObjectId(id) },
+                { $inc: { upvotes: vote } },
+                { returnDocument: "after" }
+            );
+
+        if (!feedback) {
+            return res.status(404).send({ message: "Feedback not found" });
+        }
+
+        res.json(feedback.upvotes);
+    } catch (error) {
+        console.error("Failed to update vote:", error);
+        res.status(500).send({ message: "Failed to update vote" });
+    }
 });
 
 export default router;
